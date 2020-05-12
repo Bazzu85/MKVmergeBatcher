b18ce0ebbf140552681e0f0800d081314f2d1952
Bazzu85 <c.bazzucco@gmail.com>
2020-05-12 15:13:55 +0200

    prima commit

---
+++ src/obj/UserData.cs
@@ -0,0 +1,19 @@
+﻿namespace MKVmergeBatcher.src.obj
+{
+    class UserData
+    {
+        public Window windows = new Window();
+        public ModelCreator modelCreator = new ModelCreator();
+
+        public class Window
+        {
+            public int WindowWidth { get; set; }
+
+        }
+        public class ModelCreator
+        {
+            public string Name { get; set; } = "Bazzu";
+
+        }
+    }
+}
